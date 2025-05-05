3b5bbcf1d3927cfc0135d659457f07bf54b32ad3
asim-builds <asim.baral2223@gmail.com>
2025-05-05 19:25:30 +0545

    Integrated temperature sensor and readings are shown in serial monitor

--- temp_logger_v1/temp_logger_v1.ino
+++ temp_logger_v1/temp_logger_v1.ino
@@ -1,9 +1,35 @@
+
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  dht.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(2000);
 
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  // Check if readings failed
+  if (isnan(humidity)|| isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor...");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.println(humidity);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
 }
